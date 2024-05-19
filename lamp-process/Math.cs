ce0dc36e40c03f33bb3743afd853feab085dc4d4
BeratSJ <95562697+BeratSJ@users.noreply.github.com>
2024-05-19 16:34:17 +0300

    v0.0.1-beta3
    Math.cs Edited
    Converter.cs Added

---
+++ lamp-process/Math.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
+
+namespace lamp_process
+{
+    public class Math
+    {
+        public static int total;
+
+        public static int Addition(int num1, int num2)
+         {
+            total = num1 + num2;
+            return total;
+         }
+
+        public static int Subtraction(int num1,int num2)
+        {
+            total = num1 - num2;
+            return total;
+        }
+
+        public static int Multiplication(int num1, int num2)
+        { 
+            total = num1 * num2;
+            return total; 
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            total = num1 / num2;
+            return total;
+        }
+
+        public static int NumberDouble(int num1)
+        {
+            total = num1 * 2;
+            return total;
+        }
+
+        public static int HalfDivide(int num1)
+        {
+            total = num1 / 2;
+            return total;
+        }
+    }
+}
